0a5425e21029e2a8b01d45d704e483f4e4f09fcb
Victor <victorsmirnov@me.com>
2020-05-27 00:09:44 +0300

    read json

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ const leftMenu = document.querySelector('.left-menu'),
     tvShowsList = document.querySelector('.tv-shows__list'),
     modal = document.querySelector('.modal');
 
+const BASE_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
 class DBService {
+
     async getData(url) {
         const response =  await fetch(url);
         if (!response.ok) {
@@ -19,27 +21,39 @@ class DBService {
 }
 
 // Insert TV show card data to HTML block
-const renderTvShowCard = ({id, head, vote, face, backdrop}) => {
+const renderTvShowCard = ({id, original_name, vote_average, poster_path, backdrop_path}) => {
+
+    if (backdrop_path) {
+        backdrop_path = BASE_URL + backdrop_path;
+    } else {
+        backdrop_path = './img/no-poster.jpg';
+    }
+    if (poster_path) {
+        poster_path = BASE_URL + poster_path;
+    } else {
+        poster_path = './img/no-poster.jpg';
+    }
+
 
     const card = document.createElement('li');
     card.className = 'tv-shows__item'
     card.innerHTML = `
             <a href="#" class="tv-card">
-                <span class="tv-card__vote">${vote}</span>
+                <span class="tv-card__vote">${vote_average}</span>
                 <img class="tv-card__img"
-                    src="${face}"
-                    data-backdrop="${backdrop}"
-                    alt="${head}">
-                <h4 class="tv-card__head">${head}</h4>
+                    src="${poster_path}"
+                    data-backdrop="${backdrop_path}"
+                    alt="${original_name}">
+                <h4 class="tv-card__head">${original_name}</h4>
             </a>
     `;
     tvShowsList.append(card);
 };
 
 // Place TV show cards to main page
-new DBService().getData('./data/tv-shows.json')
+new DBService().getTestData()
  .then((data) => {
-    data.forEach(renderTvShowCard);
+    data.results.forEach(renderTvShowCard);
 });
 
 // open/close menu
@@ -69,7 +83,7 @@ leftMenu.addEventListener('click', event => {
 const toggleImg = event => {
     const target = event.target;
     const img = target.closest('.tv-card__img');
-    if (img != null) {
+    if (img) {
         [img.src, img.dataset.backdrop] = [img.dataset.backdrop, img.src];
     }
 };
@@ -78,14 +92,12 @@ tvShowsList.addEventListener('mouseover', toggleImg);
 tvShowsList.addEventListener('mouseout', toggleImg);
 
 tvShowsList.addEventListener('click', event => {
-    
     const target = event.target;
     const card = target.closest('.tv-card');
     if (card) {
         document.body.style.overflow = 'hidden';
         modal.classList.remove('hide');
     }
-
 });
 
 modal.addEventListener('click', event => {
@@ -96,3 +108,5 @@ modal.addEventListener('click', event => {
         modal.classList.add('hide');
     }
 });
+
+
